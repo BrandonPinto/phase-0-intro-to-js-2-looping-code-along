55efe94414dae13d0ffa4718e849e8136bb8f9b6
Brandon Pinto <b.pintony@gmail.com>
2022-07-08 16:32:01 -0400

    commit tree 4af72d0442ee7e7f329b54cf5b4fd160222af225

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Code your solutions in this file
+for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+  }
+  const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+  }
+
+  return gifts;
+}
+wrapGifts(gifts);
+function writeCards(nameArray,eventName) {
+    var myArray = [];
+    var name = "";
+    for (let i = 0; i < nameArray.length; i++) {
+        name = nameArray[i];
+        myArray.push(`Thank you, ${name}, for the wonderful ${eventName} gift!`);
+    }
+    return myArray;
+}
+writeCards(["Guadalupe","Ollie","Aki"],"surprise");
+function countDown() {
+    let countDown = 10;
+    while (countDown >= 0) {
+        console.log(countDown);
+        countDown--;
+    }
+}
